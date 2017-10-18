7de8295cd7cd479fb302626c9686fbbea418b066
mattyDunn <31864601+mattyDunn@users.noreply.github.com>
2017-10-18 13:09:30 -0600

    Putting Content Into Lists

---
+++ BookShelfs/BookShelfs/TableViewController.cs
@@ -0,0 +1,72 @@
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace BookShelfs
+{
+    public partial class TableViewController : UITableViewController
+    {
+
+        List<Book> bookList;
+        public TableViewController (IntPtr handle) : base (handle)
+        {
+            bookList = new List<Book>();
+
+            bookList.Add(new Book ()
+            {
+                Name = "Harry Potter", 
+                Author = "J.K.K Rowling", 
+                Editor = "Bloomsberry", 
+                Year = 1998
+            });
+
+			bookList.Add(new Book()
+			{
+				Name = "Harry Potter 2",
+				Author = "J.K.K Rowling",
+				Editor = "Bloomsberry",
+				Year = 1999
+			});
+
+			bookList.Add(new Book()
+			{
+				Name = "Harry Potter 3",
+				Author = "J.K.K Rowling",
+				Editor = "Bloomsberry",
+				Year = 2000
+			});
+
+        }
+
+        public override nint NumberOfSections (UITableView tableView)
+        {
+            return 1;
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return bookList.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("Book");
+
+            var data = bookList[indexPath.Row];
+
+            cell.TextLabel.Text = data.Name;
+
+            return cell;
+        }
+    }
+
+    public class Book
+    {
+        public string Name;
+        public string Author;
+        public string Editor;
+        public int Year;
+    }
+}
